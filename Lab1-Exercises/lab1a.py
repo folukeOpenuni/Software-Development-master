31829ad479a81ae1f068655a57f660f305416b3a
folukeOpenuni <faagbede@bradford.ac.uk>
2023-10-04 15:45:39 +0100

    completed lab1 exercise

---
+++ Lab1-Exercises/lab1a.py
@@ -0,0 +1,26 @@
+#question 1
+print("\ta\tb\tc")
+print("\\\\")
+print("'")
+print("\"\"\"")
+#print("C:\nin\the downward spiral")
+
+#question 2
+# Write a print statement to produce this output:
+# / \ // \\ /// \\\
+print("/ \\ // \\\\ /// \\\\\\")
+
+#question 3
+print('''This quote is from
+Irish poet Oscar Wilde:
+"Music makes one feel so romantic
+- at least it always gets on one's nerves –
+which is the same thing nowadays."''')
+
+#question 4
+print('''A "quoted" String is
+'much' better if you learn
+the rules of "escape sequences."
+Also, "" represents an empty String.
+Don't forget: use \\" instead of " !
+'' is not the same as\"''')
---
+++ Lab1-Exercises/lab1aCal.py
@@ -0,0 +1,32 @@
+def calculate(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        return num1 / num2
+    elif operator == '%':
+        return num1 % num2
+    else:
+        return "Invalid operator"
+
+# Examples of using the calculate function:
+result1 = calculate(-9, 5, '/')
+result2 = calculate(695, 20, '%')
+result3 = calculate(7, 6 * 5, '+')
+result4 = calculate(7 * 6, 5, '+')
+result5 = calculate(248 % 100, 5, '/')
+result6 = calculate(6 * 3, 9 / 4, '-')
+result7 = calculate((5 - 7) * 4, None, None)
+result8 = calculate(6, 18 % (17 - 12), '+')
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+print(result6)
+print(result7)
+print(result8)
